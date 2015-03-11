219245ab5c9a13685b536daf81afa5e5d8414d9c
simon-fu <simonok@139.com>
2015-03-11 09:59:10 +0800

    ios traversal OK, cotrolling role delay 2 seconds

---
+++ build-adv-pjauth.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+
+make
+if [ $? -eq 0 ]; then
+cp pjlib/lib/libpj-arm-apple-darwin9.a ./lib/
+cp pjlib-util/lib/libpjlib-util-arm-apple-darwin9.a ./lib/
+cp pjnath/lib/libpjnath-arm-apple-darwin9.a ./lib/
+echo "library copied to ./lib/"
+exit 0
+else
+echo "build fail !!!"
+exit -1
+fi
+
+
